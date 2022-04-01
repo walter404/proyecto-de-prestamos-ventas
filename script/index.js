6057bd011226c38c272624872d30ca7f7881b438
walter404 <walterame123@gmail.com>
2022-04-01 14:17:50 -0300

    api

--- script/index.js
+++ script/index.js
@@ -142,3 +142,26 @@ const filtrar = () => {
     guardarLocal("listaProvincias", JSON.stringify(provincias));
 }
 boton.addEventListener('click', filtrar);
+
+//apis de divisas
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector('#currency-form').onsubmit = ()=>{
+        const base = document.getElementById('currency-from').value;
+        fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // console.log(data)
+                const amount = document.querySelector("#input-amount").value;
+                const currencyTo = document.getElementById('currency-to').value;
+                const rate = data.rates[currencyTo];
+                function convert(){
+                    return amount * rate;
+                }
+                document.querySelector('.display-result').innerHTML = `${amount} ${base.toUpperCase()} equal to ${currencyTo} ${convert().toFixed(4)}`;
+            })
+        .catch((error) =>{
+            console.log("Error: ", error);
+        });
+        return false;
+    };
+});
